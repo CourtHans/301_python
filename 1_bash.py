3e2a6fb51bf09b27b1b05c20aca87c0cd0c4faf5
CourtHans <cornta@yahoo.com>
2020-09-07 15:41:22 -0400

    initial commit

---
+++ 1_bash.py
@@ -0,0 +1,12 @@
+# Script:                       Op Challenge 06
+# Author:                       Courtney Hans
+# Date of latest revision:      9/07/20
+# Purpose:                      Bash in Python
+
+# Declare variables
+
+# Declare functions
+
+# Main
+
+# End
